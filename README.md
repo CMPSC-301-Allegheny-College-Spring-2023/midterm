cc9b71711bafb15f41c4385adf0b9b06c0c1eb1a
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2023-03-14 01:25:55 -0400

    init

---
+++ README.md
@@ -0,0 +1,82 @@
+# Data Analytics (CS301) Midterm exam
+
+**Assigned** : Tuesday 14 March 2023, 9:30am
+
+**Due** : Tuesday 14 March 2023, 3:30pm
+
+In order to allow a test where it will be completed outside of our classroom in Alden Hall, certain rules of honesty must be maintained. Please read over the rules carefully. 
+
+I wish you the best.
+
+Much thanks,
+Dr. Bonham-Carter
+
+## Rules: 
+
+* Test-takers have six hours to complete the test. 
+* Open notes, Open book. 
+* Please follow the Honor Code of Allegheny College (https://sites.allegheny.edu/about/honor-code/) 
+* You are to work alone on this test. 
+* There can be no sharing of any information between test-takers.
+* In accordance with department policies, plagiarism will be tested.
+
+```
+Honor Code
+Through the Honor Code, every student pledges to be responsible for the maintenance of honesty in one’s own academic work. Allowances under the Code, such as non-proctored exams, promote individual responsibility and the integrity of the entire College community. Allegheny’s code has the distinction of being originally developed and regularly updated and approved by the student body.
+```
+
+Data reference: https://www.kaggle.com/kumarajarshi/life-expectancy-who
+
+---
+
+### Required Deliverables
+
+* Your modified source file, `src/midterm_2023.R` in which your code may be run without bugs.
+
+### Checks for GatorGrader
+
+For immediate feedback on submissions, we will be using Gator Grade to inform the of missing components in the submission. As you submit, you will notice that there is a thick red X that will change to a green check mark when all components have been included in the submission. You are encouraged to click on the red X to find a listing of the components to address.
+
+## Project Assessment
+
+The grade that a student receives on this assignment will have the following components.
+
+- **GitHub Actions CI Build Status [up to 15%]:**: For the lab repository associated with this assignment students will receive a checkmark grade if their last before-the-deadline build passes. This is only checking some baseline writing and commit requirements as well as correct running of the program. An additional reduction will given if the commit log shows a cluster of commits at the end clearly used just to pass this requirement. An addition reduction will also be given if there is no commit during lab work times. All other requirements are evaluated manually.
+
+- **Mastery of Technical Knowledge and Skills [up to 85%]**: Students will receive a portion of their assignment grade when their program implementation reveals that they have mastered all of the technical knowledge and skills developed during the completion of this assignment. As a part of this grade, the instructor will assess aspects of the programming including, but not limited to, the completeness and the correctness of the program and the use of effective source code comments.
+
+## GatorGrade
+
+You can check the baseline writing and commit requirements for this lab assignment by running department's assignment checking `gatorgrade` tool. To use `gatorgrade`, you first need to make sure you have Python3 installed (type `python --version` to check). If you do not have Python installed, please see:
+
+- [Setting Up Python on Windows](https://realpython.com/lessons/python-windows-setup/)
+- [Python 3 Installation and Setup Guide](https://realpython.com/installing-python/)
+- [How to Install Python 3 and Set Up a Local Programming Environment on Windows 10](https://www.digitalocean.com/community/tutorials/how-to-install-python-3-and-set-up-a-local-programming-environment-on-windows-10)
+
+Then, if you have not done so already, you need to install `gatorgrade`:
+
+- First, [install `pipx`](https://pypa.github.io/pipx/installation/)
+- Then, install `gatorgrade` with `pipx install gatorgrade`
+
+Finally, you can run `gatorgrade`:
+
+`gatorgrade --config config/gatorgrade.yml`
+
+## Submitting Your Work
+
+Use GitHub to submit your work. The commands are the following.
+
+```
+git add -A
+git commit -m "add meaningful commit message"
+git push
+```
+
+## Seeking Assistance
+
+* Extra resources for using markdown include;
+  + [Markdown Tidbits](https://www.youtube.com/watch?v=cdJEUAy5IyA)
+  + [Markdown Cheatsheet](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+* Do not forget to use the above git commands to push your work to the cloud for the instructor to grade your assignment. You can go to your GitHub repository using your browser to verify that your files have been submitted. Please see the TL’s or the instructor if you have any questions about assignment submission.
+
+Students who have questions about this project are to see the instructor.
---
+++ src/midterm_2023.R
@@ -0,0 +1,146 @@
+# Midterm exam :: CS301S2023
+
+## Date: 14 March 2023
+
+## Name: Add your name here
+
+rm(list = ls()) # clear out the variables from memory to make a clean execution of the code.
+
+# If you want to remove all previous plots and clear the console, run the following two lines.
+graphics.off() # clear out all plots from previous work.
+
+cat("\014") # clear the console
+
+################################
+# Q 1: Begin by loading the "tidyverse" library. Note, if you have not installed this library, then you will have to include code to install it. 
+  # TODO: add code for library
+
+################################
+# Q 2: Use the "read.table()" function to load and open the included csv data file, "LifeExpectancyData.csv". So that you are able to work with this data, be sure to assign the variable "dat" to this loaded csv dataset.
+
+  # TODO: add code to load and read a csv file
+
+################################
+# Q 3: Write code to view the loaded dataset to get familiar with the data. 
+
+  # TODO: Add code to view the dataset that we just loaded.
+
+################################
+# Q 4: Describe your observations. What is this dataset?
+
+  # TODO What is this dataset?
+
+################################
+# Q 5: Using the code below, you note that the "Status" column contains two main values (of type character or string); "Developing" and "Developed". How would you classify the kind of data do you see in the column?
+
+unique(dat$Status)
+
+  # TODO: 
+  
+################################
+# Q 6: Create two sub datasets ("deving" and "deved") from the superset ("dat").
+# -All data in "deving" for the "Developing" state of Status of the "dat" superset.
+# -All data in "deved" for the "Developed" state of Status of the "dat" superset.
+
+  # TODO: create deving dataset for the "Developing" state of Status
+
+  # TODO: create deved dataset for the "Developed" state of Status
+
+# We will be using the "corPlot()" function from the "psych" library package. If you have not yet installed this package, then you will have to do so before continuing.
+
+################################
+# Q 7: Using the text at https://cran.r-project.org/web/packages/psych/vignettes/intro.pdf, describe in clear and meaningful language what kind of results the "corPlot()" function returns? 
+
+  # TODO 
+
+################################
+# Q 8: Complete the following code to apply the forth to the twenty-second columns of "deving" to the corePlot() fuction. Note the line above this function, "dev.off()" is there to remove plots which may still be residing in memory, and would cause an error for corPlot().
+  
+  # TODO: complete the code
+
+  dev.off() # Clear away any plot already in memory. 
+  corPlot(deving)
+
+  ################################
+# Q 9: In the Life.expectancy column of the "deving" dataset, ignoring any values of 1, what is the largest value? What is the smallest value? Give the values and the variables they represent.
+
+  # TODO: highest value: associated variable
+  
+  # TODO: lowest value: associated variable
+
+################################  
+# Q 10: According to the "deving" dataset, explain what these above two values imply for the associated variables?
+
+    # TODO
+
+################################  
+# Q 11: Complete the following code to apply the forth to the twenty-second columns of "deved" to the corePlot(). Note the line above this function, "dev.off()" is there to remove plots which may still be residing in memory, and would cause an error for corPlot().
+
+  # TODO: complete the code
+  
+  dev.off() # Clear away any plot already in memory. 
+  corPlot(deved)
+
+################################
+# Q 12: In the "Life.expectancy" column of the "deved" dataset, ignoring any values of 1, what is the largest value? What is the smallest value? Give the values and the variables they represent.
+
+  # TODO: highest value: associated variable
+  
+  # TODO: lowest value: associated variable
+
+################################    
+# Q 13: According to the "deved" dataset, explain what these above two values imply?  
+  
+  # TODO
+  
+# We want to compare the "Life.expectancy" data of two countries (Afghanistan and Bahamas) of the "Deving" dataset. The comparison will be a t-test.
+
+################################
+# Q 14: What is the null hypothesis of our test?
+
+  # TODO: Ho
+
+################################
+# Q 15: What is the alternative hypothesis of our test?
+
+  # TODO: Ha
+
+################################
+# Q 16: Correct the following code to set up the two datasets for the test. 
+
+  # TODO: Complete the code
+  
+deving_Afgh_LE <- dat | filter(Status="Developing" Country="Afghanistan") | select(Life.expectancy)
+
+deving_Baha_LE <- dat | filter(Status="Developing" Country="Bahamas") | select(Life.expectancy)
+
+t-test(deving_Afgh_LE| [deving_Baha_LE])
+
+################################
+# Q 17: What is the output of your corrected code from above? (paste in output from the test)
+
+  # TODO: output
+
+################################
+# Q 18: Which hypothesis do we conclude? In clear and meaningful language, please justify your decision.
+
+  # TODO
+
+# You suspect that there is a large difference between Year by Life.expectancy in both of the "deving" and "deved" datasets. You first need a plot to confirm your thinking.
+
+################################
+# Q 19: Prepare a ggplot scatter plot of the "deving" and "deved" datasets to be shown together on a single canvas. The points from the "deving" are to be shown in RED, and the points for the "deved" dataset are to be shown in BLUE. For each dataset, let x correspond to "Year" and y to describe the data of the "Life.expectancy" column.
+
+dev.off()
+# TODO: ggplot of Year versus Life.Expectancy for deving vs deved
+
+################################
+# Q 20: Modify this code to show the same type of plot where you let x correspond to "Year" and y to describe the data of the "Adult.Mortality" column.
+
+
+dev.off()
+# TODO: ggplot of Year versus Adult.Mortality for deving vs deved
+
+# (Did you remember to leave your name at the top of this code?)
+
+######################################################
